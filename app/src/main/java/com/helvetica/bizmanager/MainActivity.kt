aa929b74f9129be3b2e992bcfa705293f75e59e8
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-08-19 17:58:17 +0200

    night mode implemented (with bug)

--- app/src/main/java/com/helvetica/bizmanager/MainActivity.kt
+++ app/src/main/java/com/helvetica/bizmanager/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.helvetica.bizmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.helvetica.bizmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var isLight = true
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        findViewById<View>(R.id.btnNightMode).setOnClickListener {
+            if (isLight) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            isLight = !isLight
+        }
     }
 }
---
+++ app/src/main/java/com/helvetica/bizmanager/MainActivityViewModel.kt
@@ -0,0 +1,9 @@
+package com.helvetica.bizmanager
+
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+}
