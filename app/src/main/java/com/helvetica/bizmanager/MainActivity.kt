278a8734704c77f04f3bd3f6c43bfe33b4390f23
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-08-21 22:25:00 +0200

    added sidebar

--- app/src/main/java/com/helvetica/bizmanager/MainActivity.kt
+++ app/src/main/java/com/helvetica/bizmanager/MainActivity.kt
@@ -3,7 +3,10 @@ package com.helvetica.bizmanager
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
@@ -15,10 +18,36 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.miItem1 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Item 1",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.miItem2 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Item 2",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.miItem3 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Item 3",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            true
+        }
         val appSetingPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
         val sharedPrefsEdit: SharedPreferences.Editor = appSetingPrefs.edit()
         val isLight: Boolean = appSetingPrefs.getBoolean("LightMode", true)
@@ -59,9 +88,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.btnMedewerker.setOnClickListener {
-            Intent(this,SecondActivity::class.java).also {
+            Intent(this, SecondActivity::class.java).also {
                 startActivity(it)
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
