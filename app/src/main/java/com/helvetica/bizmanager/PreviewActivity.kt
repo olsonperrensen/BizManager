e570ffa236823468aa782b0e7380a02539f00150
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-08-22 17:48:40 +0200

    formatting

--- app/src/main/java/com/helvetica/bizmanager/PreviewActivity.kt
+++ app/src/main/java/com/helvetica/bizmanager/PreviewActivity.kt
@@ -10,7 +10,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.helvetica.bizmanager.repository.Repository
 
 class PreviewActivity : AppCompatActivity() {
-    private lateinit var viewModel : PreviewViewModel
+    private lateinit var viewModel: PreviewViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
@@ -18,11 +18,12 @@ class PreviewActivity : AppCompatActivity() {
         val viewModelFactory = PreviewViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory)[PreviewViewModel::class.java]
         viewModel.getHeroku()
-        viewModel.myHerokuResponse.observe(this){response->
+        viewModel.myHerokuResponse.observe(this) { response ->
             if (response.isSuccessful) {
                 Toast.makeText(this, "Backend Up!", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Error ${response.code()} backend down", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Error ${response.code()} backend down", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
         supportActionBar?.hide()
