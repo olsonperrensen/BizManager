6fbedef378670b2958279cbe8b6461c50568cfc4
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-08-22 18:59:27 +0200

    liking works

--- app/src/main/java/com/helvetica/bizmanager/ThirdActivity.kt
+++ app/src/main/java/com/helvetica/bizmanager/ThirdActivity.kt
@@ -3,26 +3,42 @@ package com.helvetica.bizmanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.helvetica.bizmanager.data.User
+import com.helvetica.bizmanager.data.UserViewModel
 import com.helvetica.bizmanager.databinding.ActivityThirdBinding
 
 class ThirdActivity : AppCompatActivity() {
     private lateinit var binding: ActivityThirdBinding
+    private lateinit var userViewModel: UserViewModel
+
+    //    FIELDS FOR DB
+    private lateinit var img: String
+    private lateinit var id: String
+    private lateinit var username: String
+    private lateinit var password: String
+    private lateinit var naam: String
+    private lateinit var land: String
+    private lateinit var sbu: String
+    private lateinit var manager: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_third)
+        userViewModel = ViewModelProvider(this)[UserViewModel::class.java]
         val intent = intent
-        val img = intent.getStringExtra("img")
-        val id = intent.getStringExtra("id")
-        val username = intent.getStringExtra("username")
-        val password = intent.getStringExtra("password")
-        val naam = intent.getStringExtra("naam")
-        val land = intent.getStringExtra("land")
-        val sbu = intent.getStringExtra("sbu")
-        val manager = intent.getStringExtra("manager")
+        img = intent.getStringExtra("img").toString()
+        id = intent.getStringExtra("id").toString()
+        username = intent.getStringExtra("username").toString()
+        password = intent.getStringExtra("password").toString()
+        naam = intent.getStringExtra("naam").toString()
+        land = intent.getStringExtra("land").toString()
+        sbu = intent.getStringExtra("sbu").toString()
+        manager = intent.getStringExtra("manager").toString()
         Glide.with(this).load(
             img
         ).fitCenter().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE)
@@ -36,8 +52,48 @@ class ThirdActivity : AppCompatActivity() {
                 "\tsbu:\t\t\t $sbu\n\n" +
                 "\tmanager:\t\t\t $manager\n\n"
         binding.fab.setOnClickListener {
-            Toast.makeText(this,"Employee liked!",Toast.LENGTH_LONG).show()
-            finish()
+            insertDataToDatabase()
         }
     }
+
+    private fun insertDataToDatabase() {
+        if (inputCheck(
+                img,
+                id,
+                username,
+                password,
+                naam,
+                land,
+                sbu,
+                manager
+            )
+        ) {
+            val user = User(0, id.toInt(), land, manager, naam, password, sbu, username)
+            userViewModel.addUser(user)
+            Toast.makeText(this, "Employee liked!", Toast.LENGTH_LONG).show()
+            finish()
+        } else {
+            Toast.makeText(this, "Please fill out all fields.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun inputCheck(
+        img: String,
+        id: String,
+        username: String,
+        password: String,
+        naam: String,
+        land: String,
+        sbu: String,
+        manager: String
+    ): Boolean {
+        return !(TextUtils.isEmpty(img) && TextUtils.isEmpty(id)
+                && TextUtils.isEmpty(username)
+                && TextUtils.isEmpty(password)
+                && TextUtils.isEmpty(naam)
+                && TextUtils.isEmpty(land)
+                && TextUtils.isEmpty(sbu)
+                && TextUtils.isEmpty(manager))
+    }
+
 }
